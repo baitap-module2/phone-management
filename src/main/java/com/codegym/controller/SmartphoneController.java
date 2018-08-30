8a5e1903a90e4eafed7ccb01d03a0a38527ab45c
vietson6103@gmail.com <Hn0912388240>
2018-08-30 10:28:53 +0700

    ...

--- src/main/java/com/codegym/controller/SmartphoneController.java
+++ src/main/java/com/codegym/controller/SmartphoneController.java
@@ -1,12 +1,4 @@
 package com.codegym.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
 public class SmartphoneController{
-    @GetMapping("/")
-    public String showIndex() {
-        return "index";
-    }
 }
